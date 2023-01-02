6feeaf564de479b272597e8f551af8e266817fef
Aziz Zakiryarov <zakiryarov@hotmail.com>
2023-01-02 11:59:38 +0000

    Modified README file & build

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # microservices-users-gitops
 
-##This repository to I created to try and test ArgoCD 
+This repository I created to try and test ArgoCD 
 
 
 
 # Installation of minikube 
 
 ```bash
-
 $ sudo apt install -y curl wget apt-transport-https
 
 $ wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
@@ -16,14 +15,21 @@ $ sudo cp minikube-linux-arm64 /usr/local/bin/minikube
 $ sudo chmod +x /usr/local/bin/minikubec
 
 $ minikube version
-
 ```
 # Start minikube 
 
 ```bash
-
 $ minikube start --driver='docker'
-
 ```
 
+# Open port to login page
 
+```bash
+$ kubectl port-forward service/argocd-server --address=127.0.0.1 8888:443  -n argocd
+```
+
+# Get the password from login page
+
+```bash
+$ kubectl get secret argocd-initial-admin-secret -n argocd -o yaml
+```
--- build.sh
+++ build.sh
@@ -1,21 +1,12 @@
 # build k8s microservices-users-gitops
 
+minikube start --driver='docker'
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
+
 kubectl config set-context --current --namespace=microservices
-
-kubectl delete deployment.apps/mysql service/mysql-service
-kubectl delete deployment.apps/microservices-users-deployment service/microservices-users
-
-kubectl apply -f $PWD/k8s/base/microservices-mysql-db/deployment.yaml
-kubectl apply -f $PWD/k8s/base/microservices-mysql-db/service.yaml
-kubectl apply -f $PWD/k8s/base/microservices-mysql-db/secret.yaml
-kubectl apply -f $PWD/k8s/base/microservices-mysql-db/persistent-volume.yaml
-kubectl apply -f $PWD/k8s/base/microservices-mysql-db/persistent-volume-claim.yaml
-
-kubectl apply -f $PWD/k8s/base/microservices-users/deployment.yaml
-kubectl apply -f $PWD/k8s/base/microservices-users/service.yaml
-kubectl apply -f $PWD/k8s/base/microservices-users/ingress.yaml
 
 kubectl apply -f $PWD/k8s/base/microservices-agrocd/namespace.yaml
 kubectl apply -n argocd -f $PWD/k8s/base/microservices-agrocd/install.yaml
 
-kubectl apply -f $PWD/k8s/base/microservices-agrocd/application.yaml
+
