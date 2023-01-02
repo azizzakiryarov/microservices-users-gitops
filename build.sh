2a8b8d05cc6a1f0a666872d0d070567599d0c095
Aziz Zakiryarov <zakiryarov@hotmail.com>
2023-01-02 17:59:27 +0000

    Modified ingress for react

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 
 minikube start --driver='docker'
 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
-
 kubectl config set-context --current --namespace=microservices
 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f $PWD/k8s/base/microservices-agrocd/namespace.yaml
 kubectl apply -n argocd -f $PWD/k8s/base/microservices-agrocd/install.yaml
 
