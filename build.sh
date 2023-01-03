a2210903e72d00565daee53e620a938f661fc268
Aziz Zakiryarov <zakiryarov@hotmail.com>
2023-01-03 10:08:39 +0000

    Changed to correct port

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ kubectl config set-context --current --namespace=microservices
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.1/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f $PWD/k8s/base/microservices-agrocd/namespace.yaml
 kubectl apply -n argocd -f $PWD/k8s/base/microservices-agrocd/install.yaml
-kubectl apply -f $PWD/k8s/base/application.yaml -n argocd
+#kubectl apply -f $PWD/k8s/base/application.yaml -n argocd
