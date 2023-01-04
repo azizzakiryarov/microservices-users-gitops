ec47a9e2ab19c34aa8fb722404e6299c880cc87e
Aziz Zakiryarov <zakiryarov@hotmail.com>
2023-01-04 14:26:23 +0100

    Reverted jenkins yaml files

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 minikube start
 minikube addons enable ingress
 
-kubectl config set-context --current --namespace=microservices
-
 kubectl apply -f $PWD/k8s/base/microservices-agrocd/namespace.yaml
 kubectl apply -n argocd -f $PWD/k8s/base/microservices-agrocd/install.yaml
+
+kubectl config set-context --current --namespace=microservices
+
