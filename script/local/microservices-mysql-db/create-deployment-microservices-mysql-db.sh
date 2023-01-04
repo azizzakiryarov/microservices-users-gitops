b8a0deaae08da81b85f9d9b42442bc615ab70555
Aziz Zakiryarov <zakiryarov@hotmail.com>
2023-01-04 10:28:10 +0100

    Modified shell script.

--- script/local/microservices-mysql-db/create-deployment-microservices-mysql-db.sh
+++ script/local/microservices-mysql-db/create-deployment-microservices-mysql-db.sh
@@ -1,9 +1,9 @@
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/secret.yaml
+kubectl apply -f $PWD/k8s/base/microservices-mysql-db/deployment.yaml
 
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/deployment.yaml
+kubectl apply -f $PWD/k8s/base/microservices-mysql-db/service.yaml
 
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/service.yaml
+kubectl apply -f $PWD/k8s/base/microservices-mysql-db/secret.yaml
 
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/persistent-volume.yaml
+kubectl apply -f $PWD/k8s/base/microservices-mysql-db/persistent-volume.yaml
 
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/persistent-volume-claim.yaml
+kubectl apply -f $PWD/k8s/base/microservices-mysql-db/persistent-volume-claim.yaml
--- script/local/microservices-users/create-deployment-microservices-users.sh
+++ script/local/microservices-users/create-deployment-microservices-users.sh
@@ -1,3 +1,5 @@
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-users/deployment.yaml
+kubectl apply -f $PWD/k8s/base/microservices-users/deployment.yaml
 
-kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-users/service.yaml
+kubectl apply -f $PWD/k8s/base/microservices-users/service.yaml
+
+kubectl apply -f $PWD/k8s/base/microservices-users/ingress.yaml
